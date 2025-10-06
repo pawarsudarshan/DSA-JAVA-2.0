b047d817b35db8446d993a705e3dfbc7f11fd27f
Sudarshan <sudarshanp4912@gmail.com>
2025-10-06 23:56:45 +0530

    Added Rotate Array problem

---
+++ src/FindTheDuplicateNumber.java
@@ -0,0 +1,13 @@
+public class FindTheDuplicateNumber {
+    public static void main(String[] args) {
+        int [] nums = {1,3,4,2,2};
+//        System.out.println(findDuplicate(nums));
+    }
+//    public static int findDuplicate(int[] nums) {
+//        int n = nums.length;
+//
+//        for(int i=0;i<n;i++){
+//
+//        }
+//    }
+}
