fbc551e45a36e567039c678e43b98499163e7dfb
Sudarshan <sudarshanp4912@gmail.com>
2025-10-07 00:13:33 +0530

    Added FindTheDuplicateNumber problem

--- src/FindTheDuplicateNumber.java
+++ src/FindTheDuplicateNumber.java
@@ -1,13 +1,24 @@
 public class FindTheDuplicateNumber {
     public static void main(String[] args) {
         int [] nums = {1,3,4,2,2};
-//        System.out.println(findDuplicate(nums));
+        System.out.println(findDuplicate(nums));
     }
-//    public static int findDuplicate(int[] nums) {
-//        int n = nums.length;
-//
-//        for(int i=0;i<n;i++){
-//
-//        }
-//    }
+
+    public static int findDuplicate(int[] nums) {
+        int n = nums.length;
+        int slow = nums[0], fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(fast!=slow);
+
+        slow = nums[0];
+            while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+            return slow;
+        }
 }
