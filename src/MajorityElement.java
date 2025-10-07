4c5d2e0665c3ff6667610996219f59eeec28829f
Sudarshan <sudarshanp4912@gmail.com>
2025-10-08 00:36:49 +0530

    Added Majority Element problem

---
+++ src/MajorityElement.java
@@ -0,0 +1,24 @@
+public class MajorityElement {
+
+    public static void main(String[] args) {
+        int [] nums = {3,2,3};
+        System.out.println(majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+        int candidate = nums[0], count = 1;
+
+        for(int i=1;i<nums.length;i++){
+            if(candidate == nums[i]) count++;
+            else{
+                count--;
+                if(count==0){
+                    candidate = nums[i];
+                    count = 1;
+                }
+            }
+        }
+
+        return candidate;
+    }
+}
