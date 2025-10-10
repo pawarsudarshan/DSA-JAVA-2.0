3ca2dda3467c4de39bbfde9aa466edd548c8592c
sudarshan-suryakant-pawar <sudarshan-suryakant.pawar@capgemini.com>
2025-10-10 16:04:53 +0530

    Added RemoveDuplicates from sorted Array problem

---
+++ src/DSA2/arrays2/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,24 @@
+package DSA2.arrays2;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,2,3,4};
+        System.out.println(removeDuplicates(arr));
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if(nums.length==1) return 1;
+        int start = 1, count = 1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]!=nums[i]){
+                count++;
+                nums[start] = nums[i];
+                start++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/DSA2/linkedlist/RotateLLbyNPlaces.java
@@ -0,0 +1,38 @@
+package DSA2.linkedlist;
+
+public class RotateLLbyNPlaces {
+    public static void main(String[] args) {
+        ListNode head = LinkedListUtils.getSampleLinkedList();
+        LinkedListUtils.printLinkedList(head);
+        ListNode newHead = rotateRight(head,2);
+        LinkedListUtils.printLinkedList(newHead);
+    }
+
+    public static ListNode rotateRight(ListNode head, int k) {
+        if(head==null || head.next==null) return head;
+        ListNode curr = head;
+        int count = 0;
+
+        while(curr!=null) {
+            if (curr.next == null) {
+                count++;
+                curr.next = head;
+                break;
+            }
+            curr = curr.next;
+            count++;
+        }
+
+        k = k%count;
+        int n = count -k;
+        curr = head;
+
+        for(int i=1;i<n;i++){
+            curr = curr.next;
+        }
+        ListNode ans = curr.next;
+        curr.next = null;
+
+        return ans;
+    }
+}
