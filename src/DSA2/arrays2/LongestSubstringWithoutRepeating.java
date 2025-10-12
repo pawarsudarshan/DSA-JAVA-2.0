bbeb16f3028cbabaffc90b0571aa1e97f91e7ca7
Sudarshan <sudarshanp4912@gmail.com>
2025-10-12 18:22:01 +0530

    Added LongestSubstring without repeat

---
+++ src/DSA2/arrays2/LongestSubstringWithoutRepeating.java
@@ -0,0 +1,30 @@
+package DSA2.arrays2;
+
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeating {
+    public static void main(String[] args) {
+        String s = "ababc";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        int maxLength = 0, n = s.length(), i =0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(int j=0;j<n;j++){
+            if(map.containsKey(s.charAt(j))){
+                while(s.charAt(i)!=s.charAt(j)){
+                    map.remove(s.charAt(i));
+                    i++;
+                }
+                i++;
+                map.put(s.charAt(j),1);
+            }else{
+                map.put(s.charAt(j),1);
+            }
+//            System.out.println("i= "+i+" j="+j);
+            if(j-i+1 > maxLength) maxLength = j-i+1;
+        }
+        return maxLength;
+    }
+}
