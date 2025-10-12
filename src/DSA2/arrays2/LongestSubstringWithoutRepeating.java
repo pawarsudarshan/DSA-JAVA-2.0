eaeb63be1dc20b1450dfbd65a84ae058ccf6673b
Sudarshan <sudarshanp4912@gmail.com>
2025-10-12 19:04:46 +0530

    Added solution using set for longest substring problem

--- src/DSA2/arrays2/LongestSubstringWithoutRepeating.java
+++ src/DSA2/arrays2/LongestSubstringWithoutRepeating.java
@@ -8,6 +8,7 @@ public class LongestSubstringWithoutRepeating {
         System.out.println(lengthOfLongestSubstring(s));
     }
     public static int lengthOfLongestSubstring(String s) {
+        // Solution 1 - using HashMap
         int maxLength = 0, n = s.length(), i =0;
         HashMap<Character, Integer> map = new HashMap<>();
 
@@ -26,5 +27,22 @@ public class LongestSubstringWithoutRepeating {
             if(j-i+1 > maxLength) maxLength = j-i+1;
         }
         return maxLength;
+
+        // Solution 2 - using sets
+//        int maxLength = 0, n = s.length(), i =0;
+//        Set<Character> set = new HashSet<>();
+//        for(int j=0;j<n;j++){
+//            if(set.contains(s.charAt(j))){
+//                while(s.charAt(i)!=s.charAt(j)){
+//                    set.remove(s.charAt(i));
+//                    i++;
+//                }
+//                i++;
+//            }else{
+//                set.add(s.charAt(j));
+//            }
+//            if(j-i+1 > maxLength) maxLength = j-i+1;
+//        }
+//        return maxLength;
     }
 }
