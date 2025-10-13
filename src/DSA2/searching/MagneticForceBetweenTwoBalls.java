800458b09d1d71c9af9d49722a9dd5043f6d08d5
sudarshan-suryakant-pawar <sudarshan-suryakant.pawar@capgemini.com>
2025-10-13 16:12:19 +0530

    Added Magnetic Force Between Two Balls problem

---
+++ src/DSA2/searching/MagneticForceBetweenTwoBalls.java
@@ -0,0 +1,41 @@
+package DSA2.searching;
+
+import java.util.Arrays;
+
+public class MagneticForceBetweenTwoBalls {
+    public static void main(String[] args) {
+        int[] pos = {1,2,3,4,7};
+        int m = 3;
+        System.out.println(maxDistance(pos,m));
+    }
+    public static int maxDistance(int[] pos, int m) {
+        Arrays.sort(pos);
+        int n = pos.length, ans = 0;
+        int start = 0, end = pos[n-1];
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            // check
+            if(check(pos,m,mid)) {
+                if(mid>ans) ans = mid;
+                start = mid + 1;
+            }else end = mid-1;
+        }
+        return ans;
+    }
+
+    public static boolean check(int[] pos, int m, int mid){
+        int n = pos.length, count = 1, prev = pos[0];
+//        System.out.println("mid: "+mid);
+        for(int i=1;i<n;i++){
+            if(pos[i]-prev >=mid){
+                prev = pos[i];
+                count++;
+            }
+            if(count==m) return true;
+        }
+//        System.out.println("mid -out: "+mid);
+        return false;
+    }
+}
