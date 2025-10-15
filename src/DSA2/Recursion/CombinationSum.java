17e4f39a4a9f888a6dbba9b02fe2263f7c335b03
sudarshan-suryakant-pawar <sudarshan-suryakant.pawar@capgemini.com>
2025-10-15 19:46:14 +0530

    Added Combination Sum problem

--- src/DSA2/Recursion/CombinationSum.java
+++ src/DSA2/Recursion/CombinationSum.java
@@ -3,30 +3,30 @@ import java.util.*;
 
 public class CombinationSum {
     public static void main(String[] args) {
-//        int[] candidates = {2,3,6,7};
-//        int target = 7;
-//        System.out.println(combinationSum(candidates, target));
+        int[] candidates = {2,3,6,7};
+        int target = 7;
+        System.out.println(combinationSum(candidates, target));
     }
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
         int n = candidates.length;
         List<List<Integer>> finalList = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        solve(candidates, target, n-1, 0,  list, finalList);
+        solve(candidates, target, 0, 0,  list, finalList);
         return finalList;
     }
 
     public static void solve(int[] arr, int target, int index, int sum, List<Integer> solution, List<List<Integer>> finalList){
         System.out.println("Index = "+index+" Sum = "+sum);
         if(sum==target){
-            finalList.add(solution);
+            finalList.add(new ArrayList<>(solution));
             return;
         }
-        if(sum>target) return;
-        if(index<0) return;
+        if(index>=arr.length || sum>target) return;
 
-        solve(arr,target,index-1, sum, solution, finalList);
+        solve(arr,target,index+1, sum, solution, finalList);
+
         solution.add(arr[index]);
         solve(arr,target,index, sum+arr[index], solution, finalList);
-        solution.remove(arr[index]);
+        solution.remove(solution.size()-1); // Backtrack
     }
 }
