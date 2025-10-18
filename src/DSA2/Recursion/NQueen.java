140444137a892f9f71655fdc221afec7b04e8c05
sudarshan-suryakant-pawar <sudarshan-suryakant.pawar@capgemini.com>
2025-10-18 22:40:24 +0530

    Added nQueen problem statement without solution

---
+++ src/DSA2/Recursion/NQueen.java
@@ -0,0 +1,32 @@
+//package DSA2.Recursion;
+//import java.util.*;
+///*
+//=> Lengthy problem, will do it later
+//*/
+//
+public class NQueen {
+//    public static void main(String[] args) {
+//        int n = 4;
+//        System.out.println(solveNQueens(n));
+//    }
+//    public static List<List<String>> solveNQueens(int n) {
+//        List<List<String>> allConfigs = new ArrayList<>();
+//        List<String> board = new ArrayList<>();
+//        for(int row=0;row<n;row++){
+//            StringBuilder sb = new StringBuilder();
+//            for(int col=0;col<n;col++){
+//                sb.append('.');
+//            }
+//            board.add(sb.toString());
+//        }
+//        System.out.println(board);
+//        return allConfigs;
+//    }
+//
+//    public static boolean solve(List<String> board, int row, int col, int n){
+//
+//    }
+//    public static boolean isValid(List<String> board, int row, int col){
+//
+    }
+//}
