8a48a887175b4b725df7b1644777c28bb6b95bb3
Sudarshan <sudarshanp4912@gmail.com>
2025-10-22 19:29:11 +0530

    Added Search in BST problem

---
+++ src/DSA2/BinaryTree/SearchInBST.java
@@ -0,0 +1,12 @@
+package DSA2.BinaryTree;
+
+public class SearchInBST {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null) return null;
+        if(root.val==val) return root;
+        TreeNode left = searchBST(root.left,val);
+        TreeNode right = searchBST(root.right,val);
+        if(left!=null) return left;
+        return right;
+    }
+}
