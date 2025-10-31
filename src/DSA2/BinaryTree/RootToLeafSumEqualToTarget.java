f854e04397394ea9a49e88defc54aab47cd6065b
sudarshan-suryakant-pawar <sudarshan-suryakant.pawar@capgemini.com>
2025-10-31 10:49:34 +0530

    Added optimized solution for root to leaf path sum equal to target

--- src/DSA2/BinaryTree/RootToLeafSumEqualToTarget.java
+++ src/DSA2/BinaryTree/RootToLeafSumEqualToTarget.java
@@ -45,4 +45,18 @@ public class RootToLeafSumEqualToTarget {
 
         sum -= root.val;
     }
+    /*
+    Clean code solution:
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null)
+            return false;
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        int remaining = targetSum - root.val;
+        return hasPathSum(root.left, remaining) || hasPathSum(root.right, remaining);
+    }
+     */
+
+
 }
