5dd3f66ef0ca0e4bfbc027677b65612b741aa9a6
sudarshan-suryakant-pawar <sudarshan-suryakant.pawar@capgemini.com>
2025-10-31 10:46:25 +0530

    Added Root to leaf Path sum equal to target problem

---
+++ src/DSA2/BinaryTree/RootToLeafSumEqualToTarget.java
@@ -0,0 +1,48 @@
+package DSA2.BinaryTree;
+
+public class RootToLeafSumEqualToTarget {
+    public static void main(String[] args) {
+        /*
+                 1
+                / \
+               2   3
+              / \   \
+             4   5   6
+        */
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+
+        int targetSum = 10;
+        System.out.println(hasPathSum(root,targetSum));
+    }
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        int[] res = new int[1];
+        solve(root,targetSum,0,res);
+        return res[0] != 0;
+    }
+
+    public static void solve(TreeNode root, int targetSum, int sum, int[] res){
+        if(root==null) return; // this base case can be omitted by adding the same bc in parent method
+
+        sum += root.val;
+
+        // check for pathSum == target => this works as the base case as well
+        if(root.left==null && root.right==null){
+            if(sum==targetSum) res[0] = 1;
+            return;
+        }
+
+        if(root.left!=null)
+            solve(root.left,targetSum,sum,res);
+
+        if(root.right!=null)
+            solve(root.right,targetSum,sum,res);
+
+        sum -= root.val;
+    }
+}
