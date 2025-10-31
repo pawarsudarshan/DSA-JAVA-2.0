d606c8d1e72f863f850fc9f51400819060a26c00
sudarshan-suryakant-pawar <sudarshan-suryakant.pawar@capgemini.com>
2025-10-31 16:31:47 +0530

    Added Top View of Binary Tree problem

---
+++ src/DSA2/BinaryTree/TopViewOfBT.java
@@ -0,0 +1,67 @@
+package DSA2.BinaryTree;
+import java.util.*;
+/*
+My first approach is to use two lists, posCol & negCol to track +ve and -ve column values separately
+*/
+public class TopViewOfBT {
+    public static void main(String[] args) {
+        /*
+                 1
+                / \
+               2   3
+              / \   \
+             4   5   6
+        */
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+
+        List<Integer> topView = topViewOfBT(root);
+        System.out.println(topView);
+    }
+    public static List<Integer> topViewOfBT(TreeNode root){
+        List<Pair> posCol = new ArrayList<>();
+        List<Pair> negCol = new ArrayList<>();
+
+        solve(root,0,0,posCol,negCol);
+
+        List<Integer> topView = new ArrayList<>();
+        for(int i=negCol.size()-1;i>=0;i--){
+            topView.add(negCol.get(i).second);
+        }
+        for(Pair p:posCol){
+            topView.add(p.second);
+        }
+
+        return topView;
+    }
+
+    public static void solve(TreeNode root, int level, int col, List<Pair> posCol, List<Pair> negCol){
+        if(root==null) return;
+
+        if(col>=0){
+            while(posCol.size()<col+1){
+                posCol.add(new Pair(level,root.val));
+            }
+            if(posCol.get(col).first>level){
+                posCol.get(col).first = level;
+                posCol.get(col).second = root.val;
+            }
+        } else{
+            int newCol = (-1)*col;
+            while(negCol.size()<newCol){
+                negCol.add(new Pair(level,root.val));
+            }
+            if(negCol.get(newCol-1).first>level){
+                negCol.get(newCol-1).first = level;
+                negCol.get(newCol-1).second = root.val;
+            }
+        }
+
+        solve(root.left,level+1,col-1, posCol, negCol);
+        solve(root.right,level+1,col+1, posCol, negCol);
+    }
+}
