b4b3c92db92337b538d1e86f4e9f8be8ebb6a749
Sudarshan <sudarshanp4912@gmail.com>
2025-11-02 11:10:58 +0530

    Added recursive approach to sort a stack problem

--- src/DSA2/Stack/SortStack.java
+++ src/DSA2/Stack/SortStack.java
@@ -1,9 +1,27 @@
 package DSA2.Stack;
 import java.util.*;
-/*
-
+/* Using Recursion:
+1. pop top element and save it
+2. sort the remaining stack using recursion
+3. place the saved top element at it's correct position in sorted stack
  */
 public class SortStack {
+    /* Using Recursion */
+    public void sort(Stack<Integer> st){
+        if(st.isEmpty()) return;
+
+        int top = st.pop();
+        sort(st);
+
+        Stack<Integer> st2 = new Stack<>();
+        while(!st.isEmpty() && st.peek()>top){
+            st2.add(st.pop());
+        }
+        st.add(top);
+        while(!st2.isEmpty()){
+            st.add(st2.pop());
+        }
+    }
     /* Sorting a stack using a helper stack and output stack */
     public void sortStack(Stack<Integer> st) {
         Stack<Integer> output = new Stack<>();
