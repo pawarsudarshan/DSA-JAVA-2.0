2ffce8a704976f16620c74f3f6c03f13f40612a8
Sudarshan <sudarshanp4912@gmail.com>
2025-11-06 23:23:31 +0530

    Added Find a pair with given sum in BST problem

---
+++ src/DSA2/BinaryTree/FindAPairWithGivenSumInBST.java
@@ -0,0 +1,23 @@
+package DSA2.BinaryTree;
+import java.util.*;
+
+public class FindAPairWithGivenSumInBST {
+    public boolean findTarget(TreeNode root, int k) {
+        boolean[] res = new boolean[1];
+        HashSet<Integer> s = new HashSet<>();
+        solve(root,k,s,res);
+        return res[0];
+    }
+    public void solve(TreeNode root, int k, HashSet<Integer> s, boolean[] res){
+        if(root==null) return;
+
+        if(s.contains(k-root.val)){
+            res[0] = true;
+            return;
+        }
+        s.add(root.val);
+
+        solve(root.left,k,s,res);
+        solve(root.right,k,s,res);
+    }
+}
