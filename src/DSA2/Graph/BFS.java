056c1b0e0f7976705a6d4e96171292526ccda4a5
Sudarshan <sudarshanp4912@gmail.com>
2025-11-15 19:07:38 +0530

    Added DFS & BFS of Graph problems

---
+++ src/DSA2/Graph/BFS.java
@@ -0,0 +1,43 @@
+package DSA2.Graph;
+import java.util.*;
+
+public class BFS {
+    public static void main(String[] args) {
+        int[][] adj = {{2, 3, 1}, {0}, {0, 4}, {0}, {2}};
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int[] arr : adj) {
+            List<Integer> list = new ArrayList<>();
+            for (int x : arr) {
+                list.add(x);
+            }
+            graph.add(list);
+        }
+
+        System.out.println(bfs(graph));
+    }
+    public static List<Integer> bfs(List<List<Integer>> graph) {
+        List<Integer> res = new ArrayList<>();
+        boolean[] visited = new boolean[graph.size()]; /* Faster than set bcoz of direct index access,
+        no int to Integer object conversion overhead, no hashing overhead
+        [] array provides contiguous memory */
+        Queue<Integer> q = new ArrayDeque<>(); // Faster than LinkedList implementation
+
+        q.add(0);
+        visited[0] = true;
+
+        while(!q.isEmpty()) {
+            int currNode = q.poll();
+            res.add(currNode);
+
+            for(int node:graph.get(currNode)) {
+                if(!visited[node]) {
+                    visited[node] = true;
+                    q.add(node);
+                }
+            }
+        }
+
+        return res;
+    }
+}
---
+++ src/DSA2/Graph/DFS.java
@@ -0,0 +1,44 @@
+package DSA2.Graph;
+import java.util.*;
+
+public class DFS {
+    public static void main(String[] args) {
+        int[][] adj = {{2, 3, 1}, {0}, {0, 4}, {0}, {2}};
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int[] arr : adj) {
+            List<Integer> list = new ArrayList<>();
+            for (int x : arr) {
+                list.add(x);
+            }
+            graph.add(list);
+        }
+
+        System.out.println(dfs(graph));
+    }
+
+    public static List<Integer> dfs(List<List<Integer>> graph) {
+        List<Integer> res = new ArrayList<>(); // for storing DFS of graph
+        Set<Integer> visited = new HashSet<>(); // for tracking visited nodes
+        int n = graph.size();
+
+        for(int i = 0; i < n; i++){
+            if(!visited.contains(i)){ // check if the nodes is previously explored
+                visited.add(i); // add to visited
+                res.add(i); // add to DFS
+                dfsHelper(graph,i,visited,res);
+            }
+        }
+        return res;
+    }
+
+    public static void  dfsHelper(List<List<Integer>> graph, int i, Set<Integer> visited, List<Integer> res){
+        for(int node: graph.get(i)){
+            if(!visited.contains(node)){
+                visited.add(node); // add to visited
+                res.add(node); // add to DFS
+                dfsHelper(graph,node,visited,res);
+            }
+        }
+    }
+}
