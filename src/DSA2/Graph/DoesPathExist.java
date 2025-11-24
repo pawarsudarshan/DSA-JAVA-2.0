c777cda25b306dc83c33dca81ad3f431893ad0fb
Sudarshan Pawar <sudarshanpawar@Sudarshans-MacBook-Air.local>
2025-11-24 22:59:54 +0530

    Added GfG solution for Does Path exist problem

--- src/DSA2/Graph/DoesPathExist.java
+++ src/DSA2/Graph/DoesPathExist.java
@@ -15,6 +15,31 @@ public class DoesPathExist {
             }
             graph.add(list);
         }
+    }
+//    GfG question solution
+    public static boolean checkPath(int V, int[][] edges, int src, int dest) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<V;i++){
+            graph.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        boolean[] vis = new boolean[V];
+        vis[src] = true;
+        return dfs(graph,vis,src,dest);
+    }
+    public static boolean dfs(List<List<Integer>> graph, boolean[] vis, int src, int dest){
+        if(src==dest) return true;
 
+        for(int node:graph.get(src)){
+            if(!vis[node]){
+                vis[node] = true;
+                if(dfs(graph,vis,node,dest)) return true;
+            }
+        }
+
+        return false;
     }
 }
