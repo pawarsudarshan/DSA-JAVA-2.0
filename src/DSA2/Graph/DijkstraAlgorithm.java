f39784eba10eed7d6ceeb95cb6d96fbb1afbc8e0
Sudarshan <sudarshanp4912@gmail.com>
2026-02-02 23:07:37 +0530

    Added Dijkstra's Algorithm Code

---
+++ src/DSA2/Graph/DijkstraAlgorithm.java
@@ -0,0 +1,52 @@
+package DSA2.Graph;
+import java.util.*;
+
+public class DijkstraAlgorithm {
+    class Solution {
+        public int[] dijkstra(int V, int[][] edges, int src) {
+            List<List<Pair>> adj = new ArrayList<>();
+            for(int i=0;i<V;i++){
+                adj.add(new ArrayList<>());
+            }
+            for(int[] edge:edges){
+                adj.get(edge[0]).add(new Pair(edge[1],edge[2]));
+                adj.get(edge[1]).add(new  Pair(edge[0],edge[2]));
+            }
+
+            PriorityQueue<DistNode> minHeap = new PriorityQueue<>((a, b) -> {
+                if (a.dist != b.dist) {
+                    return a.dist - b.dist;   // smaller dist first
+                }
+                return a.node - b.node;       // tie-breaker: smaller node first
+            });
+
+            int[] dist = new int[V];
+            Arrays.fill(dist, Integer.MAX_VALUE);
+
+            dist[src] = 0;
+            minHeap.add(new DistNode(0, src));
+
+            while(!minHeap.isEmpty()){
+                DistNode dn = minHeap.poll();
+                int distanceTillHere =  dn.dist;
+                int node = dn.node;
+                // System.out.println(distanceTillHere+" "+node);
+                for(Pair adjNode:adj.get(node)){
+                    if(adjNode.wt + distanceTillHere < dist[adjNode.node]){
+                        dist[adjNode.node] = adjNode.wt + distanceTillHere;
+                        minHeap.add(new DistNode(dist[adjNode.node], adjNode.node));
+                    }
+                }
+            }
+
+            return dist;
+        }
+    }
+}
+class DistNode{
+    int dist, node;
+    DistNode(int dist, int node){
+        this.dist = dist;
+        this.node = node;
+    }
+}
